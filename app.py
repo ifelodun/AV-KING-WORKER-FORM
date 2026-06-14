080fdab3611429b357940de68dfd82e2fe5eae3a
ifelodun <leviadeyemi02@gmail.com>
2026-06-14 17:08:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,10 +60,12 @@ os.makedirs(
 
 # Mail Settings
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = "avkingvetdrug@gmail.com"
 app.config["MAIL_PASSWORD"] = "ymzdpywlhvxpimaz"
+app.config["MAIL_TIMEOUT"] = 30
 # Initialize Extensions
 
 mail = Mail(app)
